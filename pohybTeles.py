75fccb492de5c54bb604a8f753b7405876fe8057
Ota Prochazka <103411737+ProchazkaOta@users.noreply.github.com>
2022-04-11 14:24:04 +0200

    Add files via upload

---
+++ pohybTeles.py
@@ -0,0 +1,11 @@
+# IMPORT
+import numpy
+
+# POCATECNI PODMINKY
+hmotnost_slunce = 333000 # Me (v jednotkach hmotnosti zeme)
+polomer_slunce = 0.0004*150000000 # AU (astronomicka jednotka) na km
+hmotnost_asteroidu = 2e-25 # Me
+prumer_asteroidu = 1 # km
+
+print(hmotnost_slunce)
+
