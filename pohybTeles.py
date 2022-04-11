a3c2474dce0457d0de521e9dca7277ac4c858771
ProchazkaOta <otaprochazka@icloud.com>
2022-04-11 15:48:28 +0200

    Update pohybTeles.py

--- pohybTeles.py
+++ pohybTeles.py
@@ -1,6 +1,8 @@
 # IMPORT
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from IPython.display import HTML
 
 # POCATECNI PODMINKY
 hmotnost_slunce = 333000 # Me (v jednotkach hmotnosti zeme)
@@ -10,3 +12,18 @@ prumer_asteroidu = 1 # km
 
 print(hmotnost_slunce)
 
+fig, ax = plt.subplots()
+
+
+
+ax.scatter(0, 0, marker="*", c="orange", s=1000)
+ax.scatter(0, 1, marker="o", c="black", s=100)
+#plt.show()
+
+
+
+#ax.set_xlim(( 0, 2))
+#ax.set_ylim((-2, 2))
+#ax.grid()
+
+plt.show()
